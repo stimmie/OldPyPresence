e9c16eeea423beae063113eddb8fb385bed49bbb
Sander <sander@red9.nl>
2017-10-16 19:54:53 +0200

    More logging

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ presence = False
 connected = False
 
 def on_connect(client, userdata, flags, rc):
+    logging.debug('In function')
     if rc == 0:
         logging.info('Connected to MQTT broker')
         global connected
@@ -23,6 +24,7 @@ def on_connect(client, userdata, flags, rc):
         logging.error('Connection to MQTT broker failed')
 
 def on_message(client, userdata, message):
+    logging.debug('In function')
     global state
     if message.payload == b'not_home':
         logging.debug('Message received: presence = False (not_home)')
@@ -32,6 +34,7 @@ def on_message(client, userdata, message):
         presence = True
 
 def bluetooth_ping():
+    logging.debug('In function')
     global presence
     for device, mac_address in config.presence_list.items():
         logging.debug('Starting l2ping for device: {}, MAC: {}'.format(device, mac_address))
@@ -40,18 +43,18 @@ def bluetooth_ping():
             client.publish(config.mqtt_topic_prefix + '/' + device, 'home')
             presence = True
 
-def connect_to_broker():
-
-
 if __name__ == '__main__':
     logging.info('PyPresence starting')
     client = mqttClient.Client(config.mqtt_client_name)
     client.username_pw_set(config.broker_username, password=config.broker_password)
     client.on_connect = on_connect
     client.on_message = on_message
+    logging.debug('Calling client connect')
     client.connect(config.broker_ip, port=config.broker_port)
+    logging.debug('Client start loop')
     client.loop_start()
     while connected != True:
+        logging.debug('In main while loop')
         time.sleep(0.1)
         client.subscribe(config.mqtt_topic_prefix + '/' + config.mqtt_listening_topic)
         try:
